76a7e9f1c36b2f8c971b7d8d46948f615cb70d75
Christian Page <christian.page@cerfacs.fr>
2008-07-31 06:48:36 +0000

    Updated test for classifications with sigma distributions.

--- tests/testbestclassif.c
+++ tests/testbestclassif.c
@@ -76,6 +76,11 @@ int main(int argc, char **argv)
   double *pc_eof_days = NULL;
   double *clusters = NULL;
 
+  char *fileoutclust = NULL;
+  FILE *fileoutclust_ptr = NULL;
+  char *fileoutpc = NULL;
+  FILE *fileoutpc_ptr = NULL;
+
   const gsl_rng_type *T;
   gsl_rng *rng;
 
@@ -89,6 +94,28 @@ int main(int argc, char **argv)
       (void) banner(basename(argv[0]), "OK", "END");
       return 0;
     }
+    else if ( !strcmp(argv[i], "-o_clust") ) {
+      fileoutclust = (char *) malloc(strlen((argv[++i]+1)) * sizeof(char));
+      if (fileoutclust == NULL) alloc_error(__FILE__, __LINE__);
+      (void) strcpy(fileoutclust, argv[i]);
+      fileoutclust_ptr = fopen(fileoutclust, "w");
+      if (fileoutclust_ptr == NULL) {
+        (void) fprintf(stderr, "%s: Cannot open file %s for output!\n", __FILE__, fileoutclust);
+        (void) banner(basename(argv[0]), "ABORT", "END");
+        (void) abort();
+      }
+    }
+    else if ( !strcmp(argv[i], "-o_pc") ) {
+      fileoutpc = (char *) malloc(strlen((argv[++i]+1)) * sizeof(char));
+      if (fileoutpc == NULL) alloc_error(__FILE__, __LINE__);
+      (void) strcpy(fileoutpc, argv[i]);
+      fileoutpc_ptr = fopen(fileoutpc, "w");
+      if (fileoutpc_ptr == NULL) {
+        (void) fprintf(stderr, "%s: Cannot open file %s for output!\n", __FILE__, fileoutpc);
+        (void) banner(basename(argv[0]), "ABORT", "END");
+        (void) abort();
+      }
+    }
     else {
       (void) fprintf(stderr, "%s:: Wrong arg %s.\n\n", basename(argv[0]), argv[i]);
       (void) show_usage(basename(argv[0]));
@@ -97,16 +124,16 @@ int main(int argc, char **argv)
     }
   }
 
-  /* Use simulated 10 EOFs */
-  neof = 10;
-  /* Use simulated 15000 days */
-  ndays = 15000;
+  /* Use simulated 2 EOFs */
+  neof = 2;
+  /* Use simulated 18000 days */
+  ndays = 18000;
   /* Try 1000 classifications */
   nclassif = 1000;
-  /* Use 8 clusters */
-  nclusters = 8;
+  /* Use 10 clusters */
+  nclusters = 10;
   /* Try 100 partitions for best classification */
-  npart = 10;
+  npart = 30;
 
   /* Initialize random number generator */
   T = gsl_rng_default;
@@ -120,19 +147,27 @@ int main(int argc, char **argv)
   if (clusters == NULL) alloc_error(__FILE__, __LINE__);
   
   /* Generate a double between 0.0 and 1.0 */
-  for (i=0; i<neof; i++) {
+  for (i=0; i<neof-1; i++) {
     for (j=0; j<ndays/2; j++) {
-      pc_eof_days[i+j*neof] = fabs(gsl_ran_gaussian(rng, 1.0) / 2.0);
+      pc_eof_days[i+j*neof] = gsl_ran_gaussian(rng, 1.0) + 3.0;
 #if DEBUG >= 9
       (void) fprintf(stderr, "eof %d day %d pc_eof_days %lf\n", i, j, pc_eof_days[i+j*neof]);
 #endif
     }
     for (j=ndays/2; j<ndays; j++) {
-      pc_eof_days[i+j*neof] = fabs((gsl_ran_gaussian(rng, 1.0) / 2.0))+ 0.5;
+      pc_eof_days[i+j*neof] = gsl_ran_gaussian(rng, 1.0) + 6.0;
 #if DEBUG >= 9
       (void) fprintf(stderr, "eof %d day %d pc_eof_days %lf\n", i, j, pc_eof_days[i+j*neof]);
 #endif
     }
+  }
+
+  i = neof-1;
+  for (j=0; j<ndays; j++) {
+    pc_eof_days[i+j*neof] = gsl_ran_gaussian(rng, 1.0) + 4.5;
+#if DEBUG >= 9
+    (void) fprintf(stderr, "eof %d day %d pc_eof_days %lf\n", i, j, pc_eof_days[i+j*neof]);
+#endif
   }
 
   (void) gsl_rng_free(rng);
@@ -143,11 +178,20 @@ int main(int argc, char **argv)
   /* Output data */
   for (i=0; i<neof; i++)
     for (j=0; j<nclusters; j++)
-      (void) fprintf(stdout, "%d %d %lf\n", i, j, clusters[i+j*neof]);
+      (void) fprintf(fileoutclust_ptr, "%d %d %lf\n", i, j, clusters[i+j*neof]);
+
+  for (j=0; j<ndays; j++)
+    for (i=0; i<neof; i++)
+      (void) fprintf(fileoutpc_ptr, "%d %d %lf\n", i, j, pc_eof_days[i+j*neof]);
+
+  (void) fclose(fileoutclust_ptr);
+  (void) fclose(fileoutpc_ptr);
 
   /* Free memory */
   (void) free(pc_eof_days);
   (void) free(clusters);
+  (void) free(fileoutclust);
+  (void) free(fileoutpc);
 
   /* Print END banner */
   (void) banner(basename(argv[0]), "OK", "END");
